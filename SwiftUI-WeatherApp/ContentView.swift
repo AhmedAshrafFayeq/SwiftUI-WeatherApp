b88f53a5aa73b5937cc9af8b1d1b5f0de4b2df26
Ahmed Fayek <ahmedfayek@MacBookPros-MacBook-Pro.local>
2020-12-21 21:07:18 +0200

    first commit

---
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Ahmed Fayek on 12/21/20.
+//  Copyright © 2020 Ahmed Fayek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
